aa1fd84367bdc640b582728cda5f287656ded327
Nicole Jesus <nicole_alj@hotmail.com>
2026-07-27 19:18:01 +0100

    Add inventory tests for product addition, sorting, and removal from cart

---
+++ tests/inventory.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from '../fixtures/test';
+import { users } from '../data/users';
+
+test('adds a product to the cart', async ({ loginPage, inventoryPage, cartPage }) => {
+  await test.step('Login with standard user', async () => {
+    await loginPage.open();
+    await loginPage.login(users.standard.username, users.standard.password);
+    await loginPage.expectInventoryPage();
+  });
+
+  let firstProductName = '';
+  let secondProductName = '';
+  let thirdProductName = '';
+
+  await test.step('Add two products to cart', async () => {
+    firstProductName = await inventoryPage.getProductName(0);
+    secondProductName = await inventoryPage.getProductName(1);
+    thirdProductName = await inventoryPage.getProductName(2);
+    await inventoryPage.addProductToCart(0);
+    await inventoryPage.expectCartBadge('1');
+    await inventoryPage.addProductToCart(1);
+    await inventoryPage.expectCartBadge('2');
+    await inventoryPage.addProductToCart(2);
+    await inventoryPage.expectCartBadge('3');
+    await inventoryPage.openCart();
+  });
+
+  await test.step('Validate cart', async () => {
+    await cartPage.expectCartPage();
+    await cartPage.expectProductInCart(firstProductName);
+    await cartPage.expectProductInCart(secondProductName);
+  });
+
+  await test.step('Sort products Z to A', async () => {
+    await inventoryPage.open();
+    await inventoryPage.sortProducts('za');
+    const productsAfterSort = await inventoryPage.getAllProductNames();
+    
+    // Verify products are sorted Z to A (reverse alphabetically)
+    const isSorted = productsAfterSort.every((product, i) => {
+      if (i === 0) return true;
+      return product <= productsAfterSort[i - 1];
+    });
+    expect(isSorted).toBeTruthy();
+  });
+
+  await test.step('Remove all products from cart', async () => {
+    await inventoryPage.openCart();
+    await cartPage.removeProductFromCart(firstProductName);
+    await cartPage.removeProductFromCart(secondProductName);
+    await cartPage.removeProductFromCart(thirdProductName);
+    await cartPage.expectCartEmpty();
+  });
+});
